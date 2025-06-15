0514cd4fbbb87290c575c8457ba8819bae069c90
francisco fernandes <157524425+Franciscofernandes01@users.noreply.github.com>
2025-06-14 21:31:40 -0300

    atualizacao html

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const cors = require("cors");
 const routes = require("./router/routes");
+const path = require("path");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(routes);
+
+// Define a pasta de arquivos estáticos
+app.use(express.static(path.join(__dirname, '../')));
+
+// Rota principal exibindo o index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../index.html'));
+});
 
 app.get("/", (req, res) => {
   res.send("API de Música com Autenticação JWT rodando!");
--- src/router/routes.js
+++ src/router/routes.js
@@ -7,6 +7,7 @@ const users = [];
 const autenticarJWT = (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1];
   if (!token) return res.status(403).send("Token não fornecido.");
+  
   try {
     const dados = jwt.verify(token, "segredoJWT");
     req.user = dados;
@@ -27,7 +28,7 @@ router.post("/register", (req, res) => {
   const { email, password } = req.body;
   const user = { email, password };
   users.push(user);
-  res.status(201).send("User registered");
+  res.status(201).json({ message: "User registered" });
 });
 
 router.post("/login", (req, res) => {
@@ -37,7 +38,7 @@ router.post("/login", (req, res) => {
     const token = jwt.sign({ email }, "segredoJWT", { expiresIn: "1h" });
     return res.json({ token });
   }
-  res.status(401).send("Credenciais inválidas.");
+  res.status(401).json("Credenciais inválidas.");
 });
 
 module.exports = router;
